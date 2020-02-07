02c6507145219ae3ae8379f263b382a92a9eda21
Jack Henry <43527047+SpookiSank@users.noreply.github.com>
2020-02-07 10:42:41 +0000

    Code for Game
    Uploaded on 07/02/2020.  Got the tiles to be their own object and map working.  Can apply a tower to a map.  Map is divided in the grid.

---
+++ LevelManagement.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class LevelManagement : Singleton <LevelManagement>
+{
+    [SerializeField]
+    private GameObject[] tiles;
+
+
+    public float TileWidth
+    {
+    get { return tiles[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;}
+    }
+
+    [SerializeField]
+    private CameraMovement cameraMovement;
+
+
+    [SerializeField]
+    private Transform map;
+    // Start is called before the first frame update
+
+    private Point blueSpawn;
+    private Point redSpawn;
+
+
+    [SerializeField]
+    private GameObject bluePortalPre;
+
+
+    [SerializeField]
+    private GameObject redPortalPre;
+    public Dictionary<Point, TileScript> Tiles { get; set; }
+
+
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+
+    private void CreateLevel()
+    {
+        Tiles = new Dictionary<Point, TileScript>();
+
+        string[] mapData = ReadLevelTxt();
+     
+
+        int mapXSize = mapData[0].ToCharArray().Length;
+        int mapYSize = mapData.Length;
+
+        Vector3 maxTile = Vector3.zero;
+
+        Vector3 startPosition = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        for(int y = 0; y < mapYSize; y++)
+        {
+            char[] newTile = mapData[y].ToCharArray();
+            for(int x = 0; x < mapXSize; x++)
+            {
+
+             SetTile(newTile[x].ToString() ,x,y, startPosition);
+
+            }
+
+        }
+        maxTile = Tiles[new Point(mapXSize - 1, mapYSize - 1)].transform.position;
+        cameraMovement.CameraLimits(new Vector3(maxTile.x + TileWidth, maxTile.y - TileWidth));
+
+        SpawnPortal();
+    }
+
+    private void SetTile(string tileType, int x, int y, Vector3 startPosition)
+    {
+        int tileIndex = int.Parse(tileType);
+
+        TileScript newTile = Instantiate(tiles[tileIndex]).GetComponent<TileScript>();
+  
+
+        newTile.SetUpGrid(new Point(x, y), new Vector3(startPosition.x + (TileWidth * x), startPosition.y - (TileWidth * y), 0), map);
+        
+
+       
+
+     
+    }
+
+    private string[] ReadLevelTxt()
+    {
+        TextAsset data = Resources.Load("Level1") as TextAsset;
+        string tempData = data.text.Replace(Environment.NewLine, string.Empty);
+
+        return tempData.Split('-');
+
+    }
+
+    private void SpawnPortal()
+    {
+        blueSpawn = new Point(0, 0);
+        Instantiate(bluePortalPre, Tiles[blueSpawn].GetComponent<TileScript>().WorldPosition, Quaternion.identity);
+
+        redSpawn = new Point(11, 6);
+        Instantiate(redPortalPre, Tiles[redSpawn].GetComponent<TileScript>().WorldPosition, Quaternion.identity);
+    }
+        
+       
+}
---
+++ TileScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+
+    public Point GridPosition { get; private set; }
+
+    public Vector2 WorldPosition
+    {
+        get
+        {
+            return new Vector2(transform.position.x + (GetComponent<SpriteRenderer>().bounds.size.x / 2), transform.position.y - (GetComponent<SpriteRenderer>().bounds.size.y / 2));
+        }
+
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetUpGrid(Point gridPosition, Vector3 worldPosition, Transform parent)
+    {
+        this.GridPosition = gridPosition;
+        transform.position = worldPosition;
+        transform.SetParent(parent);
+        LevelManagement.Instance.Tiles.Add(gridPosition, this);
+    
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            PlaceTower();
+        }
+        
+    }
+
+    private void PlaceTower()
+    {
+       GameObject tower = Instantiate(GameManagment.Instance.TowerPre, transform.position, Quaternion.identity);
+        tower.GetComponent<SpriteRenderer>().sortingOrder = GridPosition.Y;
+
+        tower.transform.SetParent(transform);
+    }
+}
